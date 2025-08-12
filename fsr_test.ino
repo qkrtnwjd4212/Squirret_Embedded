c74d9f336f55f1ee0e1d219cfe668ce8c5d9ba16
Sujeong Park <80205505+qkrtnwjd4212@users.noreply.github.com>
2025-08-12 17:28:22 +0900

    FSR 데이터 스프링 서버로 전송하는 로직 추가

--- fsr_test.ino
+++ fsr_test.ino
@@ -1,4 +1,15 @@
 #include "driver/rtc_io.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+// ------------------- 사용자 설정 -------------------
+const char* ssid = "*";       // 와이파이 이름
+const char* password = "*"; // 와이파이 비밀번호
+
+// 스프링 부트 서버의 주소. (PC의 IP 주소로 변경)
+const char* serverUrl = "http://*:8080/api/fsr_data"; 
+// ----------------------------------------------------
 
 // GPIO 핀 매핑
 #define FSR1_PIN 36  // ADC1_0
@@ -14,8 +25,16 @@ void setup() {
   rtc_gpio_deinit(GPIO_NUM_32); // RTC 기능 해제, 일반 GPIO로 사용 
   rtc_gpio_deinit(GPIO_NUM_33);
 
-  delay(1000);               // 안정화 시간
-  Serial.println("FSR Sensor Readings (6 Sensors):");
+  // 와이파이 연결 시작
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP()); // ESP32의 IP 주소 출력
 }
 
 void loop() {
@@ -35,38 +54,56 @@ void loop() {
   float voltage5 = fsr5_value * (3.3 / 4095.0);
   float voltage6 = fsr6_value * (3.3 / 4095.0);
 
-  // 시리얼 모니터에 출력
+  // 시리얼 모니터에 현재 값 출력
   Serial.println("------------------------------");
-  Serial.print("FSR 1 (GPIO36): ");
-  Serial.print(fsr1_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage1);
-
-  Serial.print("FSR 2 (GPIO39): ");
-  Serial.print(fsr2_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage2);
-
-  Serial.print("FSR 3 (GPIO34): ");
-  Serial.print(fsr3_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage3);
-
-  Serial.print("FSR 4 (GPIO35): ");
-  Serial.print(fsr4_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage4);
-
-  Serial.print("FSR 5 (GPIO32): ");
-  Serial.print(fsr5_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage5);
-
-  Serial.print("FSR 6 (GPIO33): ");
-  Serial.print(fsr6_value);
-  Serial.print(" | Voltage: ");
-  Serial.println(voltage6);
+  Serial.print("FSR 1 (GPIO36): "); Serial.print(fsr1_value); Serial.print(" | Voltage: "); Serial.println(voltage1);
+  Serial.print("FSR 2 (GPIO39): "); Serial.print(fsr2_value); Serial.print(" | Voltage: "); Serial.println(voltage2);
+  Serial.print("FSR 3 (GPIO34): "); Serial.print(fsr3_value); Serial.print(" | Voltage: "); Serial.println(voltage3);
+  Serial.print("FSR 4 (GPIO35): "); Serial.print(fsr4_value); Serial.print(" | Voltage: "); Serial.println(voltage4);
+  Serial.print("FSR 5 (GPIO32): "); Serial.print(fsr5_value); Serial.print(" | Voltage: "); Serial.println(voltage5);
+  Serial.print("FSR 6 (GPIO33): "); Serial.print(fsr6_value); Serial.print(" | Voltage: "); Serial.println(voltage6);
   Serial.println("------------------------------");
 
-  delay(500);  // 0.5초 간격으로 업데이트
+  // JSON 데이터 생성 (전압 값을 담도록 수정)
+  JsonDocument doc;
+  doc["voltage1"] = voltage1;
+  doc["voltage2"] = voltage2;
+  doc["voltage3"] = voltage3;
+  doc["voltage4"] = voltage4;
+  doc["voltage5"] = voltage5;
+  doc["voltage6"] = voltage6;
+
+  String jsonString;
+  serializeJson(doc, jsonString);
+
+  // HTTP POST 요청 보내기
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.println("Sending data to server...");
+    Serial.println(jsonString); // 보내는 JSON 내용 확인
+
+    int httpResponseCode = http.POST(jsonString);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      Serial.print("Response from server: ");
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected. No data sent.");
+  }
+  
+  // 1초마다 데이터 전송
+  delay(1000); 
 }
